8e5fa9257871117cc08ee708d565e446e5f2a2b2
Wesley Chen <35469114+wes-chen@users.noreply.github.com>
2021-01-12 12:28:45 -0800

    reorg + add python boilerplate

---
+++ py/bodynoline.py
@@ -0,0 +1,19 @@
+import dominate
+from dominate.tags import *
+
+def _bodynoline():
+    """Returns a body section with no vertical lines. TODO: edit this description
+
+    TODO: A description here, according to Google's Python style guide https://github.com/google/styleguide/blob/gh-pages/pyguide.md#38-comments-and-docstrings
+
+    Args:
+        TODO
+
+    Returns:
+        TODO
+
+    Raises:
+        TODO
+    """
+
+    return p('This is the body with a no line.')
