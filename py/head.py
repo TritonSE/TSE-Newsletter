6861b8f8387705641a64fa1e4deddaa5e977e0ef
Elias Fang <eliasfang@gmail.com>
2021-01-15 13:44:13 -0800

    Edit head.py docs

--- py/head.py
+++ py/head.py
@@ -2,18 +2,27 @@ import dominate
 from dominate.tags import *
 
 def _head():
-    """Returns the generated HTML header. TODO: edit this description
-
-    TODO: A description here, according to Google's Python style guide https://github.com/google/styleguide/blob/gh-pages/pyguide.md#38-comments-and-docstrings
+    """This function generates a header using tables and returns it as HTML. DO NOT EDIT!
 
     Args:
-        TODO
+        None
 
     Returns:
-        TODO
+        The generated HTML header
 
     Raises:
-        TODO
+        None
     """
 
-    return h1('This is the header.')
+    with doc:
+        h1("This is a sample table that has one row, two columns.")
+        h2("The first column has styling for a vertical yellow line.")
+        h2("The second column has another table inside of it with two rows.")
+        with table().add(tbody()).add(tr()):
+            td(div(style='border-left: 3px solid #FFD807; height: 100%', cls='myClass'))
+            with td():
+                with table().add(tbody()):
+                    tr("this is the first row of my inner table")
+                    tr("this is the second row of my inner table")
+
+    print(doc)
