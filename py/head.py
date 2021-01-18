9da012d7d651807f1c5e141e3d982cceb01cd0e0
Elias Fang <eliasfang@gmail.com>
2021-01-18 00:19:36 -0800

    Translate center tags to divs and fix css path and remove inline styles in header, greetings, and footer HTML

--- py/head.py
+++ py/head.py
@@ -1,6 +1,13 @@
 import dominate
 from dominate.tags import *
 
+# Remove when finished testing
+doc = dominate.document(title='Sample Table Description')
+
+# Remove when finished testing
+with doc.head:
+    link(rel='stylesheet', href='newsletter.css')
+
 def _head():
     """This function generates a header using tables and returns it as HTML. DO NOT EDIT!
 
@@ -13,16 +20,9 @@ def _head():
     Raises:
         None
     """
+# Add to function when finished testing (indent)
+with doc:
+    with div(style='text-align: center;', cls='ef-wrapper'):
+        h1("TEST")
 
-    with doc:
-        h1("This is a sample table that has one row, two columns.")
-        h2("The first column has styling for a vertical yellow line.")
-        h2("The second column has another table inside of it with two rows.")
-        with table().add(tbody()).add(tr()):
-            td(div(style='border-left: 3px solid #FFD807; height: 100%', cls='myClass'))
-            with td():
-                with table().add(tbody()):
-                    tr("this is the first row of my inner table")
-                    tr("this is the second row of my inner table")
-
-    print(doc)
+print(doc)
