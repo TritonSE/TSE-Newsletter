72800d2fba10a79573f5ed583b47b16ffcb8f5cf
Elias Fang <eliasfang@gmail.com>
2021-01-18 20:10:55 -0800

    Edit description and fix connection with newsletter.py

--- py/head.py
+++ py/head.py
@@ -11,47 +11,44 @@ def _head():
         None
 
     Returns:
-        The generated HTML header. The header consists of table elements split into two rows. The top row contains the banner and the
-        bottom row contains the social media handles. Within each row are more tables and columns containing the contents with anchor tags, 
-        namely the lightbulbs, title, social media icons, and social media handles.
+        An HTML document object containing the newsletter's header. The header consists of table elements split into two rows. 
+        The top row contains the banner and the bottom row contains the social media handles. Within each row are more tables 
+        and columns containing the contents with anchor tags, namely the lightbulbs, title, social media icons, and social media handles.
 
     Raises:
         None
     """
-    with doc:
-        # Outer layers
-        with div(cls='ef-wrapper').add(div(cls='ef-webkit')).add(table(cls='ef-table ef-outer')):
-            # Header
-            # Logo
-            with tr().add(td(cls='ef-td')).add(table(cls='ef-table ef-header')).add(tr()).add(td(cls='ef-td ef-logo')).add(a(href='https://tse.ucsd.edu/', target='_blank')):
-                # Left lightbulb
-                with table(cls='ef-table ef-column ef-columnLogo').add(tr()).add(td(cls='ef-td ef-padding')).add(table(cls='ef-table ef-content')).add(tr()).add(td(cls='ef-td')):
-                    img(cls='ef-lightbulb', src='https://drive.google.com/uc?export=view&id=1gq2ARk8HOCKw9MBrUfrEYe3GJjHGyBta')
-                # TSE
-                with table(cls='ef-table ef-column ef-columnLogo').add(tr()).add(td(cls='ef-td ef-padding')).add(table(cls='ef-table ef-content')).add(tr()).add(th()):
-                    h1('TRITON SOFTWARE ENGINEERING')
-                # Right lightbulb
-                with table(cls='ef-table ef-column ef-columnLogo').add(tr()).add(td(cls='ef-td ef-padding')).add(table(cls='ef-table ef-content')).add(tr()).add(td(cls='ef-td')):
-                    img(cls='ef-lightbulb', src='https://drive.google.com/uc?export=view&id=1gq2ARk8HOCKw9MBrUfrEYe3GJjHGyBta')
-            # Socials
-            with tr().add(td(cls='ef-td')).add(table(cls='ef-table ef-socials')).add(tr()).add(td(cls='ef-td ef-social')):
-                # Facebook
-                with a(href='https://www.facebook.com/TritonSE/', target='_blank').add(table(cls='ef-table ef-column ef-columnLeft')).add(tr()).add(td(cls='ef-td')).add(table(cls='ef-table ef-content')).add(tr()):
-                    with td(cls='ef-td'):
-                        img(cls='ef-icon', src='https://drive.google.com/uc?export=view&id=12a0jjtP5y-3BoSyxWQFXPEfli5rC9RyI')
-                    with td(cls='ef-td'):
-                        p('@TritonSE')
-                # Instagram
-                with a(href='https://www.instagram.com/ucsd_tse/', target='_blank').add(table(cls='ef-table ef-column ef-columnLeft')).add(tr()).add(td(cls='ef-td')).add(table(cls='ef-table ef-content')).add(tr()):
-                    with td(cls='ef-td'):
-                        img(cls='ef-icon', src='https://drive.google.com/uc?export=view&id=1pzLNGWWHnNxMgPXKl8UQREI4TblB6o-Y')
-                    with td(cls='ef-td'):
-                        p('@ucsd_tse')
-                # LinkedIn
-                with a(href='https://www.linkedin.com/company/tritonsoftwareengineering/', target='_blank').add(table(cls='ef-table ef-column ef-columnRight')).add(tr()).add(td(cls='ef-td')).add(table(cls='ef-table ef-content')).add(tr()):
-                    with td(cls='ef-td'):
-                        img(cls='ef-icon', src='https://drive.google.com/uc?export=view&id=1vEoRqm-IqkHbhpHsSV5y0k6o_LlnG-hC')
-                    with td(cls='ef-td'):
-                        p('@Triton Software Engineering')
+    with div() as header:
+        # Logo
+        with tr().add(td(cls='ef-td')).add(table(cls='ef-table ef-header')).add(tr()).add(td(cls='ef-td ef-logo')).add(a(href='https://tse.ucsd.edu/', target='_blank')):
+            # Left lightbulb
+            with table(cls='ef-table ef-column ef-columnLogo').add(tr()).add(td(cls='ef-td ef-padding')).add(table(cls='ef-table ef-content')).add(tr()).add(td(cls='ef-td')):
+                img(cls='ef-lightbulb', src='https://drive.google.com/uc?export=view&id=1gq2ARk8HOCKw9MBrUfrEYe3GJjHGyBta')
+            # TSE
+            with table(cls='ef-table ef-column ef-columnLogo').add(tr()).add(td(cls='ef-td ef-padding')).add(table(cls='ef-table ef-content')).add(tr()).add(th()):
+                h1('TRITON SOFTWARE ENGINEERING')
+            # Right lightbulb
+            with table(cls='ef-table ef-column ef-columnLogo').add(tr()).add(td(cls='ef-td ef-padding')).add(table(cls='ef-table ef-content')).add(tr()).add(td(cls='ef-td')):
+                img(cls='ef-lightbulb', src='https://drive.google.com/uc?export=view&id=1gq2ARk8HOCKw9MBrUfrEYe3GJjHGyBta')
+        # Socials
+        with tr().add(td(cls='ef-td')).add(table(cls='ef-table ef-socials')).add(tr()).add(td(cls='ef-td ef-social')):
+            # Facebook
+            with a(href='https://www.facebook.com/TritonSE/', target='_blank').add(table(cls='ef-table ef-column ef-columnLeft')).add(tr()).add(td(cls='ef-td')).add(table(cls='ef-table ef-content')).add(tr()):
+                with td(cls='ef-td'):
+                    img(cls='ef-icon', src='https://drive.google.com/uc?export=view&id=12a0jjtP5y-3BoSyxWQFXPEfli5rC9RyI')
+                with td(cls='ef-td'):
+                    p('@TritonSE')
+            # Instagram
+            with a(href='https://www.instagram.com/ucsd_tse/', target='_blank').add(table(cls='ef-table ef-column ef-columnLeft')).add(tr()).add(td(cls='ef-td')).add(table(cls='ef-table ef-content')).add(tr()):
+                with td(cls='ef-td'):
+                    img(cls='ef-icon', src='https://drive.google.com/uc?export=view&id=1pzLNGWWHnNxMgPXKl8UQREI4TblB6o-Y')
+                with td(cls='ef-td'):
+                    p('@ucsd_tse')
+            # LinkedIn
+            with a(href='https://www.linkedin.com/company/tritonsoftwareengineering/', target='_blank').add(table(cls='ef-table ef-column ef-columnRight')).add(tr()).add(td(cls='ef-td')).add(table(cls='ef-table ef-content')).add(tr()):
+                with td(cls='ef-td'):
+                    img(cls='ef-icon', src='https://drive.google.com/uc?export=view&id=1vEoRqm-IqkHbhpHsSV5y0k6o_LlnG-hC')
+                with td(cls='ef-td'):
+                    p('@Triton Software Engineering')
 
-    return doc
+    return header
