752e079a9dbd121cfb960619648df238bca107a0
Shravan Konduru <42595673+shravan1k@users.noreply.github.com>
2021-01-20 09:59:00 -0800

    change naming convention

--- py/entry_objects.py
+++ py/entry_objects.py
@@ -1,6 +1,10 @@
-class Upcoming_event:
+class UpcomingEvent:
     def __init__(self, title, details, description):
         self.title = title
         self.details = details
         self.description = description
     
+class Milestones:
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
